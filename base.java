2a2599afe49ac1568c199f00fbe611fcee192931
Moradi1412 <moradi.javid@gmail.com>
2022-10-27 23:41:31 -0700

    java

---
+++ base.java
@@ -0,0 +1,14 @@
+public class base
+{
+   public void method1()
+   {
+      System.out.println("Base");
+   }
+}
+class Sub extends base
+{
+   public void method1()
+   {
+      System.out.println("Sub");
+   }
+}
---
+++ test.java
@@ -0,0 +1,52 @@
+public class A
+{
+   private int a;
+   public A(int a)
+   {
+      this.a = a;
+   }
+}  
+
+//////////////////////////////////////////////////
+class B extends A
+{
+   private int b;
+   public B(int b, int a)
+   {
+      super(a);
+      this.b = b;
+   }
+   public void m1()
+   {
+      System.out.println("My super class is the class A");
+     
+   }
+}
+
+//////////////////////////////////////////////////////////////
+class C extends B
+{
+   private int c;
+   public C(int c, int b, int a)
+   {
+      super(b, a);
+      this.c = c;
+   }
+   public void m1()
+   {
+      System.out.println("My super class is the class B");
+      super.m1();
+   }
+}
+
+/////////////////////////////////////////////////////////////////////////
+class Driver
+{
+   public static void main(String[] args)
+ {
+      A c = new C(1,2,3);    
+      c.class.m1(); 
+  
+       
+   }
+}
